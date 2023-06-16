d7f8fc9d3bb9073b694b7e88ba91f92258e15196
josephmwangiG <josephmwangi.jgm@gmail.com>
2023-06-16 23:07:48 +0300

    ::

--- resources/js/components/admin/TrainingEvents.vue
+++ resources/js/components/admin/TrainingEvents.vue
@@ -1,132 +1,174 @@
 <template>
-    <div class="content">
-        <div class="page-inner">
-            <div class="page-header">
-                <h4 class="page-title">
-                    Training Events
-                </h4>
-                <ul class="breadcrumbs">
-                    <li class="nav-home">
-                        <a href="https://profilo.xyz/admin/dashboard?language=en">
-                            <i class="ri-home-2-line  bg-white"></i>
-                        </a>
-                    </li>
-                    <li class="separator">
-                        <i class="ri-arrow-right-s-line  bg-white"></i>
-                    </li>
-                    <li class="nav-item">
-                        <a class="bg-white" href="#">Training Events</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="row" v-if="store.trainingEvents != null">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="card-title">
-                                        Training Events
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mt-2 mt-lg-0">
-                                    <button class="btn btn-danger float-lg-right float-none btn-sm ml-2 mt-1"
-                                        data-href="https://profilo.xyz/admin/register/user/bulk-delete"><i
-                                            class="flaticon-interface-5"></i>
-                                        Delete</button>
-                                    <button class="btn btn-primary float-lg-right float-none btn-sm ml-2 mt-1"
-                                        data-toggle="modal" data-target="#trainingEventModal"><i class="fas fa-plus"></i>
-                                        Add Event</button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="table-responsive">
-                                        <table class="table table-striped mt-3" id="trainingEventTable">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col" class="pt-2">
-                                                        <input type="checkbox" class="" style="width:16px">
-                                                    </th>
-                                                    <th scope="col">Business Name</th>
-                                                    <th scope="col">Specialization</th>
-                                                    <th scope="col">Employees</th>
-                                                    <th scope="col">Email</th>
-                                                    <th scope="col">Phone</th>
-                                                    <td scope="col">Action</td>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr v-for="(item, index) in store.trainingEvents">
-                                                    <td>
-                                                        <input type="checkbox" class="bulk-check" style="width:16px">
-                                                    </td>
-                                                    <td>{{ item.business_name }}</td>
-                                                    <td>{{ item.specialization }}</td>
-                                                    <td>{{ item.number_of_employees }}</td>
-                                                    <td>{{ item.email }}</td>
-                                                    <td>{{ item.phone_number }}</td>
-
-
-                                                    <td>
-                                                        <div class="dropdown">
-                                                            <button class="btn btn-info btn-sm dropdown-toggle"
-                                                                type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                                                                aria-haspopup="true" aria-expanded="false">
-                                                                Actions
-                                                            </button>
-                                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                                                <button class="editbtn dropdown-item" data-toggle="modal"
-                                                                    data-target="#mailModal"
-                                                                    data-email="pefen68317@iucake.com">View</button>
-                                                                <button class="editbtn dropdown-item" data-toggle="modal"
-                                                                    data-target="#mailModal"
-                                                                    data-email="pefen68317@iucake.com">Approve
-                                                                </button>
-                                                                <button class="editbtn dropdown-item" data-toggle="modal"
-                                                                    data-target="#mailModal"
-                                                                    data-email="pefen68317@iucake.com">Edit</button>
-                                                                <button class="editbtn dropdown-item" data-toggle="modal"
-                                                                    data-target="#mailModal"
-                                                                    data-email="pefen68317@iucake.com">Delete</button>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  <div class="content">
+    <div class="page-inner">
+      <div class="page-header">
+        <h4 class="page-title">Training Events</h4>
+        <ul class="breadcrumbs">
+          <li class="nav-home">
+            <a href="https://profilo.xyz/admin/dashboard?language=en">
+              <i class="ri-home-2-line bg-white"></i>
+            </a>
+          </li>
+          <li class="separator">
+            <i class="ri-arrow-right-s-line bg-white"></i>
+          </li>
+          <li class="nav-item">
+            <a class="bg-white" href="#">Training Events</a>
+          </li>
+        </ul>
+      </div>
+      <div class="row" v-if="store.trainingEvents != null">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="card-title">Training Events</div>
                 </div>
+                <div class="col-lg-6 mt-2 mt-lg-0">
+                  <button
+                    class="btn btn-danger float-lg-right float-none btn-sm ml-2 mt-1"
+                    data-href="https://profilo.xyz/admin/register/user/bulk-delete"
+                  >
+                    <i class="flaticon-interface-5"></i> Delete
+                  </button>
+                  <button
+                    class="btn btn-primary float-lg-right float-none btn-sm ml-2 mt-1"
+                    data-toggle="modal"
+                    data-target="#trainingEventModal"
+                  >
+                    <i class="fas fa-plus"></i> Add Event
+                  </button>
+                </div>
+              </div>
             </div>
 
-            <TrainingEvent :form="form" />
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="table-responsive">
+                    <table
+                      class="table table-striped mt-3"
+                      id="trainingEventTable"
+                    >
+                      <thead>
+                        <tr>
+                          <th scope="col" class="pt-2">
+                            <input
+                              type="checkbox"
+                              class=""
+                              style="width: 16px"
+                            />
+                          </th>
+                          <th scope="col">Business Name</th>
+                          <th scope="col">Specialization</th>
+                          <th scope="col">Employees</th>
+                          <th scope="col">Email</th>
+                          <th scope="col">Phone</th>
+                          <td scope="col">Action</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr
+                          v-for="(item, index) in store.trainingEvents"
+                          :key="index"
+                        >
+                          <td>
+                            <input
+                              type="checkbox"
+                              class="bulk-check"
+                              style="width: 16px"
+                            />
+                          </td>
+                          <td>{{ item.business_name }}</td>
+                          <td>{{ item.specialization }}</td>
+                          <td>{{ item.number_of_employees }}</td>
+                          <td>{{ item.email }}</td>
+                          <td>{{ item.phone_number }}</td>
+
+                          <td>
+                            <div class="dropdown">
+                              <button
+                                class="btn btn-info btn-sm dropdown-toggle"
+                                type="button"
+                                id="dropdownMenuButton"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                              >
+                                Actions
+                              </button>
+                              <div
+                                class="dropdown-menu"
+                                aria-labelledby="dropdownMenuButton"
+                              >
+                                <button
+                                  class="editbtn dropdown-item"
+                                  data-toggle="modal"
+                                  data-target="#mailModal"
+                                  data-email="pefen68317@iucake.com"
+                                >
+                                  View
+                                </button>
+                                <button
+                                  class="editbtn dropdown-item"
+                                  data-toggle="modal"
+                                  data-target="#mailModal"
+                                  data-email="pefen68317@iucake.com"
+                                >
+                                  Approve
+                                </button>
+                                <button
+                                  class="editbtn dropdown-item"
+                                  data-toggle="modal"
+                                  data-target="#mailModal"
+                                  data-email="pefen68317@iucake.com"
+                                >
+                                  Edit
+                                </button>
+                                <button
+                                  class="editbtn dropdown-item"
+                                  data-toggle="modal"
+                                  data-target="#mailModal"
+                                  data-email="pefen68317@iucake.com"
+                                >
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <TrainingEvent :form="form" />
     </div>
+  </div>
 </template>
 <script setup>
-import { onMounted, ref } from "vue"
-import TrainingEvent from "./forms/TrainingEvent.vue"
-import DataTable from 'datatables.net-vue3';
-import DataTablesCore from 'datatables.net';
-import { useTrainingEventStore } from "@/store/event.js"
+import { onMounted, ref } from "vue";
+import TrainingEvent from "./forms/TrainingEvent.vue";
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net";
+import { useTrainingEventStore } from "@/store/event.js";
 
-const store = useTrainingEventStore()
-const form = ref({})
+const store = useTrainingEventStore();
+const form = ref({});
 
 onMounted(async () => {
-    let response = await store.getTrainingEvents()
-    $("#trainingEventTable").DataTable()
-})
+  let response = await store.getTrainingEvents();
+  $("#trainingEventTable").DataTable();
+});
 </script>
 <style scoped>
 .main-header {
-    height: 80px !important;
+  height: 80px !important;
 }
 </style>    
---
+++ resources/js/components/admin/forms/Coaching.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="coachingModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="coachingModalTitle"
+    style="display: none"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Add User</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-group col-lg-4 mt-2">
+            <span>Address/Location</span>
+            <el-input
+              style="border: black"
+              v-model="form.address"
+              size="large"
+              type="text"
+              placeholder="Address details"
+            ></el-input>
+            <span class="text-danger" v-if="errors.address">{{
+              errors.address[0]
+            }}</span>
+          </div>
+        </div>
+        <div class="modal-footer text-center">
+          <button
+            id="closeCoachingBtn"
+            data-dismiss="modal"
+            type="button"
+            class="submitBtn btn btn-primary"
+          >
+            Cancel
+          </button>
+          <button
+            @click="submitForm"
+            type="button"
+            class="submitBtn btn btn-primary"
+          >
+            Add User
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+// import { ElNotiffication } from "element-plus";
+const props = defineProps({
+  form: Object,
+});
+
+const errors = ref({});
+</script>
+<style lang="">
+</style>
